0bf8d6d26872ccc15311620de3d1fdfa02fba363
Sravan <sravan.bade1@gmail.com>
2019-02-10 16:15:23 +0530

    Design WebDriver Manager

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
 # Web-Automation-Selenium
 > UI Automation using Cucumber, Java and Selenium
 
-## File Reader Manager as Singleton Design Pattern
+## Design WebDriver Manager
 
-Sometimes it’s appropriate to have exactly one instance of a class. These are accessed by disparate objects throughout a software system, and therefore require a global point of access. In our case, we have ConfigReaderFile, which should be accessed globally. But later on in this Selenium Cucumber Framework series we will be having many more file readers. So it is better to have a File Reader Manager above all the File Readers. And it is better to make the manager class as singleton.  
-
- 
-### What is Singleton Design Patter?
-The Singleton’s purpose is to control object creation, limiting the number of objects to only one. Since there is only one Singleton instance, any instance fields of a Singleton will occur only once per class, just like static fields.
-
- 
-### How to implement Singleton Pattern?
-To implement Singleton pattern, we have different approaches but all of them have following common concepts.
-
-Private constructor to restrict instantiation of the class from other classes.
-Private static variable of the same class that is the only instance of the class.
-Public static method that returns the instance of the class, this is the global access point for outer world to get the instance of the singleton class.
+Why do we need WebDriver Manager or some called it as WebDriver Factory or Browser Factory?
+Till now we have been creating driver with in the Step file and we kind of explicitly tell our script to start Chrome Driver. Which means later if we need to change Chrome to Firefox, we have to go into every test script to change the same. Which is like bhrrhhhhhhh. On top of it our test is handling the logic of creating WebDriver . This is as equal as you visit some restaurant and you are cooking your meal by yourself instead of enjoying eating your meal. Test should not be worried about how to start the driver. Test should just need a driver to execute the script. Like you in restaurant just know what to eat. So lets bring the waiter ops I mean to say WebDriver Manager in between to handle this task for us. 
--- src/main/java/dataProviders/ConfigFileReader.java
+++ src/main/java/dataProviders/ConfigFileReader.java
@@ -6,13 +6,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ConfigFileReader {
-	
-	private Properties properties;
-	private final String propertyFilePath= "configs//Configuration.properties";
+import enums.DriverType;
+import enums.EnvironmentType;
 
-	
-	public ConfigFileReader(){
+public class ConfigFileReader {
+	private Properties properties;
+	private final String propertyFilePath = "configs//Configuration.properties";
+
+	public ConfigFileReader() {
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(propertyFilePath));
@@ -26,31 +27,96 @@ public class ConfigFileReader {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
-		}		
-	}
-	
-	public String getDriverPath(){
-		String driverPath;
-		String os = System.getProperty("os.name").toLowerCase();
-		if(os.contains("mac")) {
-    	  driverPath = properties.getProperty("macChromeDriverPath");
-		}else {
-		  driverPath = properties.getProperty("windowsChromeDriverPath");
 		}
-		if(driverPath!= null) return driverPath;
-		else throw new RuntimeException("driverPath not specified in the Configuration.properties file.");		
 	}
-	
-	public long getImplicitlyWait() {		
+
+	public String getDriverPath() {
+		String driverPath = null;
+		String os = System.getProperty("os.name").toLowerCase();
+		String browserName = properties.getProperty("browser");
+		if (os.contains("mac")) {
+			if (browserName.equals("chrome"))
+				driverPath = properties.getProperty("macChromeDriverPath");
+			else if (browserName.equals("firefox"))
+				driverPath = properties.getProperty("macFirefoxDriverPath");	
+		} else if (os.contains("windows")){
+			if (browserName.equals("chrome"))
+				driverPath = properties.getProperty("windowsChromeDriverPath");
+			else if (browserName.equals("firefox"))
+				driverPath = properties.getProperty("windowsFirefoxDriverPath");
+			else if (browserName.equals("iexplorer"))
+				driverPath = properties.getProperty("windowsIEDriverPath");
+			else if (browserName.equals("edge"))
+				driverPath = properties.getProperty("windowsEdgeDriverPath");
+		}
+		if (driverPath != null)
+			return driverPath;
+		else
+			throw new RuntimeException("driverPath not specified in the Configuration.properties file.");
+	}
+
+	/*
+	 * public long getImplicitlyWait() { String implicitlyWait =
+	 * properties.getProperty("implicitlyWait"); if(implicitlyWait != null) return
+	 * Long.parseLong(implicitlyWait); else throw new
+	 * RuntimeException("implicitlyWait not specified in the Configuration.properties file."
+	 * ); }
+	 */
+
+	public long getImplicitlyWait() {
 		String implicitlyWait = properties.getProperty("implicitlyWait");
-		if(implicitlyWait != null) return Long.parseLong(implicitlyWait);
-		else throw new RuntimeException("implicitlyWait not specified in the Configuration.properties file.");		
+		if (implicitlyWait != null) {
+			try {
+				return Long.parseLong(implicitlyWait);
+			} catch (NumberFormatException e) {
+				throw new RuntimeException("Not able to parse value : " + implicitlyWait + " in to Long");
+			}
+		}
+		return 30;
 	}
-	
+
 	public String getApplicationUrl() {
 		String url = properties.getProperty("url");
-		if(url != null) return url;
-		else throw new RuntimeException("url not specified in the Configuration.properties file.");
+		if (url != null)
+			return url;
+		else
+			throw new RuntimeException(
+					"Application Url not specified in the Configuration.properties file for the Key:url");
+	}
+
+	public DriverType getBrowser() {
+		String browserName = properties.getProperty("browser");
+		if (browserName == null || browserName.equals("chrome"))
+			return DriverType.CHROME;
+		else if (browserName.equalsIgnoreCase("firefox"))
+			return DriverType.FIREFOX;
+		else if (browserName.equals("iexplorer"))
+			return DriverType.INTERNETEXPLORER;
+		else if (browserName.equals("edge"))
+			return DriverType.EDGE;
+		else if (browserName.equals("safari"))
+			return DriverType.SAFARI;
+		else
+			throw new RuntimeException(
+					"Browser Name Key value in Configuration.properties is not matched : " + browserName);
+	}
+
+	public EnvironmentType getEnvironment() {
+		String environmentName = properties.getProperty("environment");
+		if (environmentName == null || environmentName.equalsIgnoreCase("local"))
+			return EnvironmentType.LOCAL;
+		else if (environmentName.equals("remote"))
+			return EnvironmentType.REMOTE;
+		else
+			throw new RuntimeException(
+					"Environment Type Key value in Configuration.properties is not matched : " + environmentName);
+	}
+
+	public Boolean getBrowserWindowSize() {
+		String windowSize = properties.getProperty("windowMaximize");
+		if (windowSize != null)
+			return Boolean.valueOf(windowSize);
+		return true;
 	}
 
 }
